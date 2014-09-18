a3488ed6711c503e20c5b30a3b1ad39d18c9e9a8
Jeky <jeky.cui@gmail.com>
2014-09-18 17:44:12 -0400

    finish crawl part

--- crawler.py
+++ crawler.py
@@ -8,6 +8,7 @@ class Crawler:
 
     def __init__(self):
         self.idSet = Set()
+        self.idQueue = []
         self.twitter = Twitter(APP_KEY, APP_SECRET)
         self.db = DB()
         self.cache = Cache()
@@ -18,8 +19,27 @@ class Crawler:
 
 
     def crawl(self, seed):
-        self.idSet.add(seed)
-        if not self.db.containsUser(seed):
-            self.crawlUserInfo(seed)
+        self.add(seed)
 
-        while 
+        while not self.top and len(self.idSet) != 0:
+            userId = self.getFirstInQueue()
+            if not self.db.containsUser(userId):
+                self.crawlUserInfo(userId)
+
+            if self.cache.contains(userId):
+                friendList = self.cache.get(userId)
+            else:
+                friendList = []
+                friendJson = self.twitter.getFriendList(userId)
+                for f in friendJson['ids']:
+                    friendList.append(f)
+
+
+
+    def add(self, userId):
+        self.idSet.add(userId)
+        self.idQueue.append(userId)
+
+
+    def getFirstInQueue(self):
+        return self.idQueue.pop(0)
